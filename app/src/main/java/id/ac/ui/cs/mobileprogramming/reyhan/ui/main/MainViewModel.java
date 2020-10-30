61813d301a14f0c95523d2df489a9b05f0d43982
Reyhan Alhafizal <reyhanhamidi@gmail.com>
2020-10-30 23:09:28 +0700

    Init project

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/reyhan/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.mobileprogramming.reyhan.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
