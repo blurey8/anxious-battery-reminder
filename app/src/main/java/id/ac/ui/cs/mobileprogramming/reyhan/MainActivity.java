3a738169ebee59639cde3c7e42909d3ff780de51
Reyhan Alhafizal <reyhanhamidi@gmail.com>
2020-11-16 02:03:21 +0700

    Push notification when battery drops

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/reyhan/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/reyhan/MainActivity.java
@@ -2,8 +2,11 @@ package id.ac.ui.cs.mobileprogramming.reyhan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import id.ac.ui.cs.mobileprogramming.reyhan.service.BatteryDropReminderService;
 import id.ac.ui.cs.mobileprogramming.reyhan.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
+
+        Intent service = new Intent(getApplicationContext(), BatteryDropReminderService.class);
+        getApplicationContext().startService(service);
     }
 }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/reyhan/ui/main/MainFragment.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/reyhan/ui/main/MainFragment.java
@@ -2,6 +2,11 @@ package id.ac.ui.cs.mobileprogramming.reyhan.ui.main;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,16 +16,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import id.ac.ui.cs.mobileprogramming.reyhan.R;
 
 public class MainFragment extends Fragment {
 
     private MainViewModel mViewModel;
+    private TextView mText;
 
     public static MainFragment newInstance() {
         return new MainFragment();
     }
+
+    private BroadcastReceiver mBatteryChangedReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            mText.setText("Battery level: " + String.valueOf(level) + "%");
+        }
+    };
+
 
     @Nullable
     @Override
@@ -30,10 +46,24 @@ public class MainFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mText = view.findViewById(R.id.text);
+
+        getContext().registerReceiver(mBatteryChangedReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getContext().unregisterReceiver(mBatteryChangedReceiver);
+    }
 }
